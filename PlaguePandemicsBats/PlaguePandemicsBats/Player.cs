b6771617708cf75bc35b92b7ed6c2c45d992a7a0
Diogo Figueiredo <daof2001@gmail.com>
2020-05-12 22:02:43 +0100

    Created a class Player (For testing purposes)

---
+++ PlaguePandemicsBats/PlaguePandemicsBats/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace PlaguePandemicsBats
+{
+    class Player : DrawableGameComponent
+    {
+        public Player(Game1 game) : base(game)
+        {
+
+        }
+    }
+}
