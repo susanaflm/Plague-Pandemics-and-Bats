15bcf2644c56ce0ec9156e1e9fddb6f8e01a85f3
Diogo Figueiredo <daof2001@gmail.com>
2020-06-11 19:14:31 +0100

    Added Extension class

---
+++ PlaguePandemicsBats/Extension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaguePandemicsBats
+{
+    public static class Extension
+    {
+        public static float Pos(this Vector2 vector, int i)
+        {
+            return i == 0 ? vector.X : vector.Y;
+        }
+
+        public static float DeltaTime(this GameTime gameTime)
+        {
+            return (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+    }
+}
