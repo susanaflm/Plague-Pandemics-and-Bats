73a2e8636af7d283c9701afe5932c8a5991ad1ec
dragonskaterdx666 <susiethingos@gmail.com>
2020-06-11 18:00:21 +0100

    added a lot more shit than diogo just in 1 commit

---
+++ PlaguePandemicsBats/TilingBackground.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PlaguePandemicsBats
+{
+    class TilingBackground
+    {
+        private  Texture2D _background;
+
+        private  Vector2 _realSize;
+
+        private  Game _game;
+
+        public TilingBackground(Game game, string texture, Vector2 realSize)
+        {
+            _game = game;
+
+            _realSize = realSize;
+
+            _background = game.Content.Load<Texture2D>(texture);
+
+        }
+    }
+}
